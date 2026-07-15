804627812448a074aa6ee8592583ed123934fa3e
niiimsam <nadeem.samaali@mail.mcgill.ca>
2026-07-14 22:03:20 -0400

    Updated bufferline scheme

--- lua/gruvbox-material/groups/plugins.lua
+++ lua/gruvbox-material/groups/plugins.lua
@@ -100,10 +100,8 @@ function M.get(c, config)
 		groups.NvimTreeWinSeparator = { fg = c.border, bg = c.bg_sidebar }
 	end
 
-	groups.BufferLineOffsetSeparator = { bg = c.bg }
-
 	if enabled("bufferline") then
-		groups.BufferLineBackground = { bg = c.bg }
+		groups.BufferLineOffsetSeparator = { bg = c.bg, fg = c.border }
 		groups.BufferLineTab = { fg = c.comment, bg = c.bg_tab_inactive }
 		groups.BufferLineTabSelected = { fg = c.fg, bg = c.bg_tab_active }
 		groups.BufferLineTabClose = { fg = c.error }
